4785515065dcac1544d2f0f5c9fda3b501614f44
mmcel <mm69@yahoo.com>
2025-08-25 19:50:28 -0500

    Added comments to both Lab 1 files

--- Lab_1/class_list.py
+++ Lab_1/class_list.py
@@ -1,5 +1,17 @@
-class_list = ['Software development capstone', 'ITEC career preporation']
+# Create an empty list to store the names
+class_list = []
 
+# While loop to allow for multiple inputs
+while True: 
+    # Get the naame of the class and add it to the list
+    class_name = input('Enter a class name: ')
+    # If user inputs nothing, break the loop
+    if not class_name:
+        break
+    class_list.append(class_name) 
+
+# Loop through the list and display the class names
 for course in class_list:
     print(course)
 
+                       
--- Lab_1/hello_birthday_month.py
+++ Lab_1/hello_birthday_month.py
@@ -1,11 +1,15 @@
+# Get the user's name and birthday month
 name = input("Please enter your name: ")
 birthday_month = input("Please enter your birth month: ")
 
+# Get the length of the user's name
 name_length = len(name)
 
+# Display the results
 print(f"Hello, {name}! Your name has {name_length} letters")
 
+# Check if the birthday month is August and display the appropriate message
 if birthday_month.lower() == "august":
     print("Your birthday was this month!")
 else:
-    print("Your birthday was noth this month")
+    print("Your birthday was not this month.")
