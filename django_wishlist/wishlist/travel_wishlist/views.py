57c9a53d3d28bbd472ab9a0c99cd9872a68e23ad
mmcel <mm69@yahoo.com>
2025-11-08 16:45:02 -0600

    Added a page to view places visited

--- django_wishlist/wishlist/travel_wishlist/views.py
+++ django_wishlist/wishlist/travel_wishlist/views.py
@@ -7,11 +7,26 @@ def place_list(request):
 
     if request.method == "POST":
         form = NewPlaceForm(request.POST)
-        place = form.save()
-        if form.is_valid():
-            place.save()
-            return redirect('place_list')
+        place = form.save() # Create a new place from the form data
+        if form.is_valid(): # Checks against DB contraints to ensure all required fields are present
+            place.save() # Saves the new place to the DB
+            return redirect('place_list') # Redirects to get the view with the name place_list which will now show the new place
 
+    # If not a POST request or the form is invalid, render the travel_wishlist page with a blank form to try again
     places = Place.objects.filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
+
+def places_visited(request):
+    visited = Place.objects.filter(visited=True)
+    return render(request, 'travel_wishlist/visited.html', {'visited': visited})
+
+def place_was_visited(request, place_pk):
+    if request.method == 'POST':
+        place = Place.objects.get(pk=place_pk)
+        place.visited = True
+        place.save()
+
+        return redirect('place_list')
+
+    
