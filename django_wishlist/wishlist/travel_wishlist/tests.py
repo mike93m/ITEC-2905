3a495b120d9cb8e550edc4138acbe4db9f8154ab
mmcel <mm69@yahoo.com>
2025-11-12 18:19:43 -0600

    Created first unit test. Created fixtured directory and test_places.json to store example data for tests.

--- django_wishlist/wishlist/travel_wishlist/tests.py
+++ django_wishlist/wishlist/travel_wishlist/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Place
 
 # Create your tests here.
+
+class TestHomePage(TestCase):
+
+    def test_load_home_page_shows_empty_for_empty_database(self):
+        home_page_url = reverse('place_list')
+        response = self.client.get(home_page_url)
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        self.assertContains(response, 'You have no places in your travel wishlist.')
