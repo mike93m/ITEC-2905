044e18f32b50796cf31cb56672ab986b9bb080fd
mmcel <mm69@yahoo.com>
2025-11-16 17:17:47 -0600

    Created Video class in models.py.

--- django_video/video/video_collection/models.py
+++ django_video/video/video_collection/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=400)
+    notes = models.Charfield(blank=True, null=True)
+
+    def __str__(self):
+    # String displayed in the admin console or when printing a Video object
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
