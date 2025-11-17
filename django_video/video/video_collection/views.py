adfe45f8d97f0ccc097944373e787493c6dc172e
mmcel <mm69@yahoo.com>
2025-11-16 20:42:37 -0600

    Updated add in views to handle post requests.

--- django_video/video/video_collection/views.py
+++ django_video/video/video_collection/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Video
 from .forms import VideoForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,5 +10,16 @@ def home(request):
     return render(request, 'video_collection/home.html', {'app_name': app_name})
 
 def add(request):
+    # Handle the form submission
+    if request.method == 'POST':
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            new_video_form.save()
+            messages.success(request, 'Video added successfully!')
+        else:
+            messages.warning(request, 'Error adding video. Please check the form.')
+            render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+
     new_video_form = VideoForm()
     return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
