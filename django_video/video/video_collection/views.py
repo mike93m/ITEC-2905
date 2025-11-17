7589c6481d65f4531e7c43094cb078f583f0b41b
mmcel <mm69@yahoo.com>
2025-11-16 19:50:55 -0600

    Created home page view and updated urls. Made base.html file.

--- django_video/video/video_collection/views.py
+++ django_video/video/video_collection/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    app_name = 'Fun viral videos'
+    return render(request, 'video_collection/home.html', {'app_name': app_name})
