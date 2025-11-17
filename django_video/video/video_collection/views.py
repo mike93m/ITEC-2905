8c25aadc4d8b4817bd5cf6915ab16dcf42a4a3c8
mmcel <mm69@yahoo.com>
2025-11-16 20:35:21 -0600

    Created a new view for adding a video. Updated urls, created a form and created the add.html page.

--- django_video/video/video_collection/views.py
+++ django_video/video/video_collection/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Video
+from .forms import VideoForm
 
 # Create your views here.
 
 def home(request):
     app_name = 'Fun viral videos'
     return render(request, 'video_collection/home.html', {'app_name': app_name})
+
+def add(request):
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
