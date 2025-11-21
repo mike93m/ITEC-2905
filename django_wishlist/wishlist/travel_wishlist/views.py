1451aacdbfa75e04aa2ee21b52d4f677fc1c45c7
mmcel <mm69@yahoo.com>
2025-11-21 15:01:21 -0600

    Added uder authentification to the app but now its not working.

--- django_wishlist/wishlist/travel_wishlist/views.py
+++ django_wishlist/wishlist/travel_wishlist/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def place_list(request):
 
     if request.method == "POST":
@@ -17,10 +19,12 @@ def place_list(request):
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
+@login_required
 def places_visited(request):
     visited = Place.objects.filter(visited=True)
     return render(request, 'travel_wishlist/visited.html', {'visited': visited})
 
+@login_required
 def place_was_visited(request, place_pk):
     if request.method == 'POST':
         place = get_object_or_404(Place, pk=place_pk)
